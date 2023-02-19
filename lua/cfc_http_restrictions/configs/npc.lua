c21839033189843828fbc70fe157fc8d45465bda
Redox <mail@redox.to>
2023-02-20 00:36:13 +0100

    Update npc.lua

--- lua/cfc_http_restrictions/configs/npc.lua
+++ lua/cfc_http_restrictions/configs/npc.lua
@@ -7,13 +7,14 @@ return {
         ["rpc.garrysmod.site"] = { allowed = true, permanent = true, noisy = true },
         ["npcs.gg"] = { allowed = true, permanent = true, noisy = true },
         
-		-- domains starfall docs have
+	-- domains starfall docs have / useful for starfall
         ["npms.io"] = {allowed=true},
         ["fb.me"] = {allowed=true},
         ["reactjs.org"] = {allowed=true},
         ["www.w3.org"] = {allowed=true},
+	["api.imgur.com"] = { allowed = true, permanent = true, noisy = true },
         
-		-- media player
+	-- media player
         ["samuelmaddock.github.io"] = {allowed=true, isPermanent=true} -- Media player
     }
 }
