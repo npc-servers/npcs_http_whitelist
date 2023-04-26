794ad9b94f9efec4b019fd6d9a4b02ae5966fcb3
Redox <mail@redox.to>
2023-04-26 22:34:49 +0200

    Use spaces instead of tabs

--- lua/cfc_http_restrictions/configs/npc.lua
+++ lua/cfc_http_restrictions/configs/npc.lua
@@ -6,17 +6,17 @@ return {
         ["garrysmod.site"] = { allowed = true, permanent = true, noisy = true },
         ["rpc.garrysmod.site"] = { allowed = true, permanent = true, noisy = true },
         ["npcs.gg"] = { allowed = true, permanent = true, noisy = true },
-	["gameserveranalytics.com"] = { allowed = true, permanent = true, noisy = true },
-	["api.steampowered.com"] = { allowed = true, permanent = true, noisy = true },
+    ["gameserveranalytics.com"] = { allowed = true, permanent = true, noisy = true },
+    ["api.steampowered.com"] = { allowed = true, permanent = true, noisy = true },
         
-	-- domains starfall docs have / useful for starfall
+    -- domains starfall docs have / useful for starfall
         ["npms.io"] = {allowed=true},
         ["fb.me"] = {allowed=true},
         ["reactjs.org"] = {allowed=true},
         ["www.w3.org"] = {allowed=true},
-	["api.imgur.com"] = { allowed = true, permanent = true, noisy = true },
+    ["api.imgur.com"] = { allowed = true, permanent = true, noisy = true },
         
-	-- media player
+    -- media player
         ["samuelmaddock.github.io"] = {allowed=true, isPermanent=true} -- Media player
     }
 }
