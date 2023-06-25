0cecaa680fef68a870e4ec8436ccf4f95cc60e33
Redox <mail@redox.to>
2023-06-25 13:12:28 +0200

    Style

--- lua/cfc_http_restrictions/configs/npc.lua
+++ lua/cfc_http_restrictions/configs/npc.lua
@@ -9,20 +9,20 @@ return {
         ["npcz.gg"] = { allowed = true, permanent = true, noisy = true },
         ["gameserveranalytics.com"] = { allowed = true, permanent = true, noisy = true },
         ["api.steampowered.com"] = { allowed = true, permanent = true, noisy = true },
-        
+
         -- image services
         ["i.pinimg.com"] = { allowed = true },
 
         -- domains starfall docs have / useful for starfall
-        ["npms.io"] = {allowed=true},
-        ["fb.me"] = {allowed=true},
-        ["reactjs.org"] = {allowed=true},
-        ["www.w3.org"] = {allowed=true},
+        ["npms.io"] = { allowed = true },
+        ["fb.me"] = { allowed = true },
+        ["reactjs.org"] = { allowed = true },
+        ["www.w3.org"] = { allowed = true },
         ["api.imgur.com"] = { allowed = true, permanent = true, noisy = true },
-        
+
         -- media player
-        ["samuelmaddock.github.io"] = {allowed=true}
-        ["youtube.com"] = {allowed=true}
-        ["www.youtube.com"] = {allowed=true}
+        ["samuelmaddock.github.io"] = { allowed = true },
+        ["youtube.com"] = { allowed = true },
+        ["www.youtube.com"] = { allowed = true }
     }
 }
